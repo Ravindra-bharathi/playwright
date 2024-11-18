44bb3603f1ce27c877068c422629603a80c68ecb
Ravindrabharathi <ravindrabharathi.a@basiscloudsolutions.com>
2024-11-18 18:03:59 +0530

    worked on playwirght

--- tests/appstore.spec.ts
+++ tests/appstore.spec.ts
@@ -1,86 +1,105 @@
 import { test, expect, chromium } from "@playwright/test";
-import { LoginPage } from "./components/loginPageForPredev";
-import { AppStorePage } from "./components/appstorefile";
 import { password, username } from "./variable/variable";
 
 let page;
-let appStorePage;
-
+let pageForLogin;
 test.beforeAll(async () => {
   test.setTimeout(240000);
-
   const browser = await chromium.launch({ headless: false });
   page = await browser.newPage();
-
-  const loginPage = new LoginPage(page);
-  const pageForLogin = await loginPage.login(username, password);
-
-  appStorePage = new AppStorePage(pageForLogin);
-  await appStorePage.clickAppStore();
+  await page.goto("https://predev.symphony4cloud.com/login");
+  await page.getByPlaceholder("Email, phone, or Skype").fill(username);
+  await page.getByRole("button", { name: "Next" }).click();
+  await page.locator("#i0118").fill(password);
+  await page.getByRole("button", { name: "Sign in" }).click();
+  await page.getByRole("button", { name: "Yes" }).click();
+  const pageWaitPopup = page.waitForEvent("popup");
+  await page.goto("https://predev.symphony4cloud.com/login");
+  pageForLogin = await pageWaitPopup;
+  await pageForLogin.getByLabel("").click();
+  await pageForLogin.getByRole("option", { name: "c100001" }).click();
+  await pageForLogin.getByRole("button", { name: "Sign In" }).click();
 });
 
-test("test for app store page", async () => {
-  const page1Text = await appStorePage.page.locator(
+test("test for app store page", async ({}) => {
+  await pageForLogin
+    .getByRole("button", { name: "AppStore AppStore" })
+    .first()
+    .click();
+  const page1Text = await pageForLogin.locator(
     "(//p[contains(@class,'MuiTypography-root MuiTypography-body1')])[1]"
   );
 
-  await expect(page1Text).toBeVisible();
+  // await expect(page1Text).toBeVisible();
   await page1Text.click();
-
-  await appStorePage.createCapability(
-    "tesst using playwright",
-    "Playwright",
-    "SAP"
+  const capability = await pageForLogin.getByText("Create capability");
+  await expect(capability).toBeVisible();
+  await pageForLogin
+    .locator(
+      "(//input[contains(@class,'MuiInputBase-input MuiOutlinedInput-input')])[3]"
+    )
+    .fill("tesst using playwright");
+  const appLanguageType = pageForLogin.locator(
+    "(//div[contains(@class,'MuiSelect-select MuiSelect-outlined')])[2]"
   );
+  await appLanguageType.click();
+  await pageForLogin.locator("li[role='option']").nth(1).click();
+  const appGroup = await pageForLogin.locator(
+    "//div[contains(@class,'MuiInputAdornment-root MuiInputAdornment-positionStart')]/following-sibling::div[1]"
+  );
+  await appGroup.click();
+  await pageForLogin.locator("li[role='option']").nth(1).click();
+  await pageForLogin.getByRole("button", { name: "Create" }).click();
+  const icon = await pageForLogin.locator("(//img[@class='jss381'])[2]");
 
-  await page
+  await pageForLogin
     .locator(
       "(//div[contains(@class,'MuiPaper-root MuiPaper-elevation')]//button)[2]"
     )
     .click();
 });
 
-test("test for app store search page", async () => {
-  await appStorePage.searchApp("tesst using playwright");
-  const text = await appStorePage.getSearchResult();
-  const textArr = text.toString();
-  console.log(textArr, "test for app store search page");
-  await appStorePage.searchApp("");
+test("test for app store page for paganation for next page with 12 rows", async ({}) => {
+  await pageForLogin.getByLabel("Go to next page").click();
+  await expect(pageForLogin.getByText("Ansible  - Yaml_Test")).toBeVisible();
+});
+test("test for app store page for paganation for next page with 24 rows", async ({}) => {
+  await pageForLogin.getByLabel("12").click();
+  await pageForLogin.getByRole("option", { name: "24" }).click();
+  await pageForLogin.getByLabel("Go to next page").click();
+  await expect(
+    pageForLogin.getByText("WEB IDE  - Check -AWS-SNAPSHOT")
+  ).toBeVisible();
 });
 
-test("test for app store pagination for next page with 12 rows", async () => {
-  await appStorePage.goToNextPage();
-  const text = await appStorePage.getNextPageWith12();
-  const textArr = text.toString();
-  await expect(textArr).toBe("Ansible  - Yaml_Test");
+test("test for app store page for paganation for next page with 48 rows", async ({}) => {
+  await pageForLogin.getByLabel("24").click();
+  await pageForLogin.getByRole("option", { name: "48" }).click();
+  await pageForLogin.getByLabel("Go to next page").click();
+  await expect(pageForLogin.getByText("GITHUB  - test sh git13")).toBeVisible();
 });
 
-test("test for app store pagination for next page with 24 rows", async () => {
-  await appStorePage.changeRowsPerPage(12);
-  await appStorePage.goToNextPage();
-  const text = await appStorePage.getpagniationFor24();
-  const textArr = text.toString();
-  await expect(textArr).toBe("WEB IDE  - Check -AWS-SNAPSHOT");
+test("test for app store page for paganation for next page with 100 rows", async ({}) => {
+  await pageForLogin.getByLabel("48").click();
+  await pageForLogin.getByRole("option", { name: "100" }).click();
+  await pageForLogin.getByLabel("Go to next page").click();
+  await expect(
+    pageForLogin.getByText("WEB IDE  - testingsample")
+  ).toBeVisible();
 });
 
-test("test for app store pagination for next page with 48 rows", async () => {
-  await appStorePage.changeRowsPerPage(24);
-  await appStorePage.goToNextPage();
-  const text = await appStorePage.getpagniationFor48();
-  const textArr = text.toString();
-  await expect(textArr).toBe("GITHUB  - test sh git13");
-});
-
-test("test for app store pagination until last page", async () => {
-  const count = await page.getByLabel("Next Page").count();
+test("test for app store page for paganation untill lastpage", async ({}) => {
+  const count = await pageForLogin.getByLabel("Next Page").count();
   for (let i = 0; i < count; i++) {
-    await appStorePage.goToNextPage();
+    await pageForLogin.getByLabel("Next Page").click();
   }
-  const isDisabled = await appStorePage.isNextPageDisabled();
-  await expect(isDisabled).toBeFalsy();
+  const isDisabled = await pageForLogin.getByLabel("Next Page").isDisabled();
+  await expect(isDisabled).toBeTruthy();
 });
 
 test.afterAll(async () => {
-  await appStorePage.logout();
+  await pageForLogin.getByRole("button", { name: "R", exact: true }).click();
+  await pageForLogin.getByRole("menuitem", { name: "Log Out" }).click();
+  await pageForLogin.close();
   await page.close();
 });
