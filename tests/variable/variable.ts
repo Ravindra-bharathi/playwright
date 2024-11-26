8b601936c288af4a2e9a8a5ff7309a8fd7b4600b
Ravindrabharathi <ravindrabharathi.a@basiscloudsolutions.com>
2024-11-26 13:02:42 +0530

    worked on playwirght

--- tests/variable/variable.ts
+++ tests/variable/variable.ts
@@ -1,4 +1,5 @@
 const username = "$username";
 const password = "$password";
 const url = "https://predev.symphony4cloud.com/login";
+const customerCode = "c100001";
 export { username, password, url };
