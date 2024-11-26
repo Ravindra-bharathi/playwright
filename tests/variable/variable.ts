ff58cec54dc373131c93d9b48f2956c98baee908
Ravindrabharathi <ravindrabharathi.a@basiscloudsolutions.com>
2024-11-26 12:58:03 +0530

    worked on playwirght

--- tests/variable/variable.ts
+++ tests/variable/variable.ts
@@ -2,4 +2,5 @@ const username = "$username";
 const password = "$password";
 const url = "https://predev.symphony4cloud.com/login";
 const baseUrl = "https://predev.symphony4cloud.com";
+const loginPage = `${baseUrl}/login`;
 export { username, password, url };
