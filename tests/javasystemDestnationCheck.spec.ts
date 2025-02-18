f97781dbf43ba8cbae29202d38cd4edea37bea9f
Ravindra-bharathi <ravindrabharathi.a01@gmail.com>
2025-02-18 18:41:59 +0530

    worked in excelfile at playwright

--- tests/javasystemDestnationCheck.spec.ts
+++ tests/javasystemDestnationCheck.spec.ts
@@ -1,7 +1,7 @@
 import { test, chromium, Browser, Page, BrowserContext } from "@playwright/test";
 import { HomePage } from "./javasystemDestnation/checkDestnation";
 
-test.describe('Home Page Tests', () => {
+test.describe(() => {
     test.setTimeout(100000000);
     let browser: Browser;
     let context: BrowserContext;
