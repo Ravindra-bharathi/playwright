8dab22e2cce278adf546bc533b0ec728dbb53407
Ravindra-bharathi <ravindrabharathi.a01@gmail.com>
2025-03-05 12:33:37 +0530

    salesforce at playwright

--- tests/activityCreation.spec.ts
+++ tests/activityCreation.spec.ts
@@ -1,41 +1,63 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Page } from '@playwright/test';
 import { email, firstName, lastName, leadStatus, password, url, username } from './leadCreationVariable';
 
-test("Navigate to Lead and Update Status", async ({ page }) => {
-    try {
-        await page.goto(url);
-        await page.getByRole('textbox', { name: 'Username' }).fill(username);
-        await page.getByRole('textbox', { name: 'Password' }).fill(password);
-        await page.getByRole('button', { name: 'Log In to Sandbox' }).click();
-        await page.waitForLoadState('networkidle');
+test.describe(() => {
+    test.setTimeout(100000000);
+    let page: Page;
 
-        await page.getByRole('button', { name: 'App Launcher' }).click();
-        await page.getByRole('option', { name: 'Sales', exact: true }).click();
-        await page.getByRole('link', { name: 'Leads' }).click();
-        await page.getByRole('searchbox', { name: 'Search this list...' }).fill(email);
-        await page.getByRole('searchbox', { name: 'Search this list...' }).press('Enter');
+    test.beforeAll(async ({ browser }) => {
+        page = await browser.newPage();
+    });
 
-        await page.getByRole('link', { name: `${firstName} ${lastName}` }).first().click();
-        await page.getByRole('tab', { name: 'Details' }).click();
-        await page.getByRole('button', { name: 'Edit Lead Status' }).click();
-        await page.getByRole('combobox', { name: 'Lead Status' }).click();
+    test("Navigate to Lead and Update Status", async ({ page }) => {
+        test.setTimeout(800000);
 
-        const statusLocator = page.locator(`lightning-base-combobox-item[data-value="${leadStatus}"]`);
-        if (await statusLocator.count() > 0) {
-            await statusLocator.click();
-        } else {
-            console.error('Lead status option not found!');
-            return;
+        try {
+            await page.goto(url);
+            await page.waitForTimeout(5000);
+            await page.getByRole('textbox', { name: 'Username' }).fill(username);
+            await page.waitForTimeout(2000);
+            await page.getByRole('textbox', { name: 'Password' }).fill(password);
+            await page.waitForTimeout(2000);
+            await page.getByRole('button', { name: 'Log In to Sandbox' }).click();
+            await page.waitForTimeout(5000);
+            await page.getByRole('button', { name: 'App Launcher' }).click();
+            await page.waitForTimeout(2000);
+            await page.getByRole('option', { name: 'Sales', exact: true }).click();
+            await page.waitForTimeout(2000);
+            await page.getByRole('link', { name: 'Leads' }).click();
+            await page.waitForTimeout(2000);
+            await page.getByRole('searchbox', { name: 'Search this list...' }).fill(email);
+            await page.getByRole('searchbox', { name: 'Search this list...' }).press('Enter');
+            await page.waitForTimeout(2000);
+            await page.getByRole('link', { name: `${firstName} ${lastName}` }).first().click();
+            await page.waitForTimeout(2000);
+            await page.getByRole('tab', { name: 'Details' }).click();
+            await page.waitForTimeout(2000);
+            await page.getByRole('button', { name: 'Edit Lead Status' }).click();
+            await page.waitForTimeout(2000);
+            await page.getByRole('combobox', { name: 'Lead Status' }).click();
+            await page.waitForTimeout(2000);
+            const statusLocator = page.locator(`lightning-base-combobox-item[data-value="${leadStatus}"]`);
+            if (await statusLocator.isVisible()) {
+                await statusLocator.click();
+            } else {
+                console.error('Lead Status option not found!');
+            }
+            await page.getByRole('button', { name: 'Save' }).click();
+            await page.waitForTimeout(5000);
+            await page.getByRole('tab', { name: 'Activity' }).click();
+            await page.waitForTimeout(2000);
+            await expect(page.locator('.timelineItem')).toBeVisible();
+            await page.waitForTimeout(2000);
+
+            const activity = page.getByAltText('Task', { exact: true });
+            if (await activity.isVisible()) {
+                const activityCreationStatus = "activity is created"
+                console.log(`** gbStart ** activityCreationStatus ** splitKeyValue ** ${activityCreationStatus} ** gbEnd **`);
+            }
+        } catch (error) {
+            console.error('Test failed:', error);
         }
-
-        await page.getByRole('button', { name: 'Save' }).click();
-        await page.getByRole('tab', { name: 'Activity' }).click();
-
-        const activity = page.getByAltText('Task', { exact: true });
-        await expect(activity).toBeVisible();
-
-        console.log(`**gbStart**activityCreationStatus**splitKeyValue**activity is created**gbEnd**`);
-    } catch (error) {
-        console.error('Test failed:', error);
-    }
+    });
 });
--- tests/leadCreation.spec.ts
+++ tests/leadCreation.spec.ts
@@ -1,32 +1,58 @@
-import { test, expect } from '@playwright/test';
+import { test, Page } from '@playwright/test';
 import { City, company, email, firstName, lastName, password, phone, product, salutation, url, username } from './leadCreationVariable';
 
-test("Navigate to Salesforce and Create a Lead", async ({ page }) => {
-    try {
-        await page.goto(url);
-        await page.getByRole('textbox', { name: 'Username' }).fill(username);
-        await page.getByRole('textbox', { name: 'Password' }).fill(password);
-        await page.getByRole('button', { name: 'Log In to Sandbox' }).click();
-        await page.waitForLoadState('networkidle');
-        await page.getByRole('button', { name: 'App Launcher' }).click();
-        await page.getByRole('option', { name: 'Sales', exact: true }).click();
-        await page.getByRole('link', { name: 'Leads' }).click();
-        await page.getByRole('button', { name: 'New' }).click();
-        await page.getByRole('combobox', { name: 'Salutation' }).click();
-        await page.getByText(salutation).click();
-        await page.getByRole('textbox', { name: 'First Name' }).fill(firstName);
-        await page.getByRole('textbox', { name: '*Last Name' }).fill(lastName);
-        await page.getByRole('textbox', { name: '*Company' }).fill(company);
-        await page.getByRole('textbox', { name: 'Phone' }).fill(phone);
-        await page.getByRole('textbox', { name: 'Email' }).fill(email);
-        await page.getByLabel('Available').getByText(product).click();
-        await page.getByLabel('*Product').getByRole('button', { name: 'Move to Chosen Move selection' }).click();
-        await page.getByRole('combobox', { name: 'Quantity' }).click();
-        await page.getByLabel('New Lead').getByText('1KG').click();
-        await page.getByRole('textbox', { name: 'City' }).fill(City);
-        await page.getByRole('button', { name: 'Save', exact: true }).click();
-        console.log(`**gbStart**leadCreationName**splitKeyValue**${firstName} ${lastName}**gbEnd**`);
-    } catch (error) {
-        console.error('Test failed:', error);
-    }
+test.describe(() => {
+    test.setTimeout(100000000);
+    let page: Page;
+
+    test.beforeAll(async ({ browser }) => {
+        page = await browser.newPage();
+    });
+
+    test("Navigate to Salesforce and Create a Lead", async ({ page }) => {
+        test.setTimeout(800000);
+        try {
+            await page.goto(url);
+            await page.waitForTimeout(5000);
+            await page.getByRole('textbox', { name: 'Username' }).fill(username);
+            await page.waitForTimeout(2000);
+            await page.getByRole('textbox', { name: 'Password' }).fill(password);
+            await page.waitForTimeout(2000);
+            await page.getByRole('button', { name: 'Log In to Sandbox' }).click();
+            await page.waitForTimeout(5000);
+            await page.getByRole('button', { name: 'App Launcher' }).click();
+            await page.waitForTimeout(2000);
+            await page.getByRole('option', { name: 'Sales', exact: true }).click();
+            await page.waitForTimeout(2000);
+            await page.getByRole('link', { name: 'Leads' }).click();
+            await page.waitForTimeout(2000);
+            await page.getByRole('button', { name: 'New' }).click();
+            await page.waitForTimeout(2000);
+            await page.getByRole('combobox', { name: 'Salutation' }).click();
+            await page.getByText(salutation).click();
+            await page.getByRole('textbox', { name: 'First Name' }).fill(firstName);
+            await page.getByRole('textbox', { name: '*Last Name' }).fill(lastName);
+            await page.waitForTimeout(2000);
+            await page.getByRole('textbox', { name: '*Company' }).fill(company);
+            await page.waitForTimeout(2000);
+            await page.getByRole('textbox', { name: 'Phone' }).fill(phone);
+            await page.waitForTimeout(2000);
+            await page.getByRole('textbox', { name: 'Email' }).fill(email);
+            await page.waitForTimeout(2000);
+            await page.getByLabel('Available').getByText(product).click();
+            await page.getByLabel('*Product').getByRole('button', { name: 'Move to Chosen Move selection' }).click();
+            await page.waitForTimeout(2000);
+            await page.getByRole('combobox', { name: 'Quantity' }).click();
+            await page.waitForTimeout(2000);
+            await page.getByLabel('New Lead').getByText('1KG').click();
+            await page.waitForTimeout(2000);
+            await page.getByRole('textbox', { name: 'City' }).fill(City);
+            await page.waitForTimeout(2000);
+            await page.getByRole('button', { name: 'Save', exact: true }).click();
+            await page.waitForTimeout(5000);
+            console.log(`**gbStart**leadCreationName**splitKeyValue**${firstName} ${lastName}**gbEnd**`);
+        } catch (error) {
+            console.error('Test failed:', error);
+        }
+    });
 });
