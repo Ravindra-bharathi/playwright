8579c55067c4bbef7eacdb6b326f466cded3587c
Ravindra-bharathi <ravindrabharathi.a01@gmail.com>
2025-03-10 13:12:03 +0530

    salesforce at playwright

---
+++ tests/quotationCheck.spec.ts
@@ -0,0 +1,62 @@
+import { test, Page, expect } from '@playwright/test';
+import { name, password, productName, Quantity, QuoteName, url, username } from './quotationVariable';
+
+
+test.describe(() => {
+    test.setTimeout(800000);
+    let page: Page;
+    test.beforeAll(async ({ browser }) => {
+        page = await browser.newPage();
+    });
+
+    test("Navigate to opportunity page", async () => {
+        test.setTimeout(800000);
+        test.setTimeout(800000);
+        await page.goto(url);
+        await page.waitForTimeout(5000);
+        await page.getByRole('textbox', { name: 'Username' }).click();
+        await page.getByRole('textbox', { name: 'Username' }).fill(username);
+        await page.waitForTimeout(2000);
+        await page.getByRole('textbox', { name: 'Password' }).click();
+        await page.getByRole('textbox', { name: 'Password' }).fill(password);
+        await page.waitForTimeout(2000);
+        await page.getByRole('button', { name: 'Log In to Sandbox' }).click();
+        await page.waitForTimeout(5000);
+        await page.getByRole('button', { name: 'App Launcher' }).click();
+        await page.waitForTimeout(2000);
+        await page.getByRole('option', { name: 'Sales', exact: true }).click();
+        await page.waitForTimeout(2000);
+        await page.getByRole('link', { name: 'Opportunities' }).click();
+        await page.waitForTimeout(2000);
+        await page.getByRole('link', { name: name }).click();
+        await page.getByRole('tab', { name: 'Related' }).click();
+        await page.evaluate(() => window.scrollBy(0, window.innerHeight));
+        const newQuote = page.getByRole('button', { name: 'New Quote' });
+        await newQuote.click();
+        await page.waitForTimeout(2000);
+        await page.getByRole('textbox', { name: '*Quote Name' }).click();
+        await page.getByRole('textbox', { name: '*Quote Name' }).fill(QuoteName);
+        await page.waitForTimeout(2000);
+        await page.getByRole('button', { name: 'Save' }).click();
+        await page.waitForTimeout(2000);
+        await page.getByRole('gridcell', { name: QuoteName }).getByRole('link').click();
+        await page.getByRole('button', { name: 'Add Products' }).click();
+        const search = page.getByRole('combobox', { name: 'Search <Entity> Search <' });
+        await search.waitFor();
+        await search.click();
+        await search.fill(productName);
+        await search.click();
+        await page.getByRole('option', { name: 'Soda Ash Powder 004' }).click();
+        await page.getByRole('button', { name: 'Next' }).click();
+        await page.getByRole('button', { name: 'Edit Quantity: Item null' }).click();
+        await page.getByRole('textbox', { name: 'Quantity *' }).fill(Quantity);
+        await page.waitForTimeout(2000);
+        await page.getByRole('button', { name: 'Save' }).click();
+        await expect(page.getByRole('link', { name: productName })).toBeVisible();
+
+        const product = page.getByRole('link', { name: productName })
+        if (await product.isVisible()) {
+            console.log(`**gbStart**product is added **splitKeyValue**${productName}**gbEnd**`);
+        }
+    });
+});
---
+++ tests/quotationVariable.ts
@@ -0,0 +1,7 @@
+export const url = 'https://basiscloudsolutionspvtltd--vishnudev.sandbox.my.salesforce.com/';
+export const username = 'vijayakumarp@basiscloudsolutions.com.vishnudev';
+export const password = 'Vishnudev@21';
+export const name = 'jayanthanp';
+export const QuoteName = 'test'
+export const productName = 'Soda Ash Powder'
+export const Quantity = '2'
