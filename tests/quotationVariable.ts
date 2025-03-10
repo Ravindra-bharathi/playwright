3e214c26853ed3a33f13260dab2eb6c9a3e60f81
Ravindra-bharathi <ravindrabharathi.a01@gmail.com>
2025-03-10 13:46:32 +0530

    salesforce at playwright

--- tests/quotationVariable.ts
+++ tests/quotationVariable.ts
@@ -1,8 +1,8 @@
 export const url = '$url';
-export const username = 'username';
-export const password = 'password';
+export const username = '$username';
+export const password = '$password';
 export const name = '$name';
 export const QuoteName = '$quoteName'
 export const productName = '$productName'
-export const Quantity = 'quantity'
+export const Quantity = '$quantity'
 
