6d972102989fcacfb738644eab52f0064178e70a
Ravindra-bharathi <ravindrabharathi.a01@gmail.com>
2025-03-13 13:05:33 +0530

    salesforce at playwright

--- tests/leadCreationVariable.ts
+++ tests/leadCreationVariable.ts
@@ -1,4 +1,4 @@
-export const url = '$url';
+export const url = 'https://basiscloudsolutionspvtltd--demoasset.sandbox.my.salesforce.com/?ec=301&startURL=%2Fvisualforce%2Fsession%3Furl%3Dhttps%253A%252F%252Fbasiscloudsolutionspvtltd--demoasset.sandbox.my.salesforce-setup.com%252Flightning%252Fsetup%252FObjectManager%252Fhome';
 export const username = '$username';
 export const password = '$password';
 export const firstName = '$firstname';
