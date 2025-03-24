7297cb136f0badfb0f6a1e4d0ebb0089828e8848
Ravindra-bharathi <ravindrabharathi.a01@gmail.com>
2025-03-24 19:03:00 +0530

    salesforce at playwright

--- tests/activityCreationDemo.spec.ts
+++ tests/activityCreationDemo.spec.ts
@@ -51,53 +51,53 @@ test.describe(() => {
             if (!(await status.isVisible())) {
                 const leadStatus = 'The Lead status is already qualified Or Contacted '
                 console.log(`**gbStart**leadStatus**splitKeyValue**${leadStatus}**gbEnd**`);
-                return;
             }
-
-            await page.getByRole('link', { name: `${firstName} ${lastName}` }).first().click();
-            await page.waitForTimeout(2000);
-            await page.getByRole('tab', { name: 'Details' }).click();
-            await page.waitForTimeout(2000);
-            await page.getByRole('button', { name: 'Edit Lead Status' }).click();
-            await page.waitForTimeout(2000);
-            await page.getByRole('combobox', { name: 'Lead Status' }).click();
-            await page.getByRole('option', { name: 'Contacted' }).nth(1).click();
-            await page.getByRole('textbox', { name: '*Comments' }).click();
-            await page.getByRole('textbox', { name: '*Comments' }).fill('contacted');
-            await page.getByRole('button', { name: 'Save' }).click();
-            await page.waitForTimeout(5000);
-            await page.locator('div').filter({ hasText: /^ContactedContacted$/ }).locator('a').click();
-            await page.waitForTimeout(5000);
-            await expect(page.getByLabel('Highlights panel header').getByText('Contacted')).toBeVisible();
-            await page.goBack();
-            await page.waitForTimeout(5000);
-            await page.getByRole('button', { name: 'Edit Lead Status' }).click();
-            await page.getByRole('combobox', { name: 'Lead Status' }).click();
-            await page.getByRole('option', { name: 'Qualified', exact: true }).locator('span').nth(1).click();
-            await page.getByRole('textbox', { name: '*Comments' }).click();
-            await page.getByRole('textbox', { name: '*Comments' }).fill('Qualified');
-            await page.waitForTimeout(2000);
-            await page.getByRole('button', { name: 'Save' }).click();
-            await page.waitForTimeout(5000);
-            const activity = page.getByText('You have an upcoming task', { exact: true }).first();
-            if (await activity.isVisible()) {
-                const activityCreationStatus = "activity is created"
-                console.log(`**gbStart**activityCreationStatus**splitKeyValue**${activityCreationStatus}**gbEnd**`);
-            }
-            await page.locator('div').filter({ hasText: /^QualifiedQualified$/ }).locator('a').click();
-            await page.waitForTimeout(2000);
-            await page.getByRole('heading', { name: 'Task Qualified' }).locator('span').click();
-            await page.waitForTimeout(2000);
-            await page.getByRole('link', { name: 'Opportunities' }).click();
-            await page.waitForTimeout(2000);
-            await page.getByRole('rowheader', { name: `${firstName}${lastName} Edit` }).getByRole('link').click();
-            await page.waitForTimeout(2000);
-            await expect(page.getByRole('link', { name: `${email}` })).toBeVisible();
-            await page.getByRole('link', { name: `${phone}` }).click();
-            const Opportunitie = page.getByRole('heading', { name: `Opportunity ${firstName}${lastName}` }).locator('div');
-            if (await Opportunitie.isVisible()) {
-                const opportinuteCreationStatus = "Opportinute is created"
-                console.log(`**gbStart**opportinuteCreationStatus**splitKeyValue**${opportinuteCreationStatus}**gbEnd**`);
+            else {
+                await page.getByRole('link', { name: `${firstName} ${lastName}` }).first().click();
+                await page.waitForTimeout(2000);
+                await page.getByRole('tab', { name: 'Details' }).click();
+                await page.waitForTimeout(2000);
+                await page.getByRole('button', { name: 'Edit Lead Status' }).click();
+                await page.waitForTimeout(2000);
+                await page.getByRole('combobox', { name: 'Lead Status' }).click();
+                await page.getByRole('option', { name: 'Contacted' }).nth(1).click();
+                await page.getByRole('textbox', { name: '*Comments' }).click();
+                await page.getByRole('textbox', { name: '*Comments' }).fill('contacted');
+                await page.getByRole('button', { name: 'Save' }).click();
+                await page.waitForTimeout(5000);
+                await page.locator('div').filter({ hasText: /^ContactedContacted$/ }).locator('a').click();
+                await page.waitForTimeout(5000);
+                await expect(page.getByLabel('Highlights panel header').getByText('Contacted')).toBeVisible();
+                await page.goBack();
+                await page.waitForTimeout(5000);
+                await page.getByRole('button', { name: 'Edit Lead Status' }).click();
+                await page.getByRole('combobox', { name: 'Lead Status' }).click();
+                await page.getByRole('option', { name: 'Qualified', exact: true }).locator('span').nth(1).click();
+                await page.getByRole('textbox', { name: '*Comments' }).click();
+                await page.getByRole('textbox', { name: '*Comments' }).fill('Qualified');
+                await page.waitForTimeout(2000);
+                await page.getByRole('button', { name: 'Save' }).click();
+                await page.waitForTimeout(5000);
+                const activity = page.getByText('You have an upcoming task', { exact: true }).first();
+                if (await activity.isVisible()) {
+                    const activityCreationStatus = "activity is created"
+                    console.log(`**gbStart**activityCreationStatus**splitKeyValue**${activityCreationStatus}**gbEnd**`);
+                }
+                await page.locator('div').filter({ hasText: /^QualifiedQualified$/ }).locator('a').click();
+                await page.waitForTimeout(2000);
+                await page.getByRole('heading', { name: 'Task Qualified' }).locator('span').click();
+                await page.waitForTimeout(2000);
+                await page.getByRole('link', { name: 'Opportunities' }).click();
+                await page.waitForTimeout(2000);
+                await page.getByRole('rowheader', { name: `${firstName}${lastName} Edit` }).getByRole('link').click();
+                await page.waitForTimeout(2000);
+                await expect(page.getByRole('link', { name: `${email}` })).toBeVisible();
+                await page.getByRole('link', { name: `${phone}` }).click();
+                const Opportunitie = page.getByRole('heading', { name: `Opportunity ${firstName}${lastName}` }).locator('div');
+                if (await Opportunitie.isVisible()) {
+                    const opportinuteCreationStatus = "Opportinute is created"
+                    console.log(`**gbStart**opportinuteCreationStatus**splitKeyValue**${opportinuteCreationStatus}**gbEnd**`);
+                }
             }
         } catch (error) {
             console.error('Test failed:', error);
