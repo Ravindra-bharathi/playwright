4fe60a9ba0e6c2430c740c5f4db60a46658758a1
Ravindra-bharathi <ravindrabharathi.a01@gmail.com>
2025-04-26 11:45:10 +0530

    salesforce at playwright

--- tests/floating_variable.ts
+++ tests/floating_variable.ts
@@ -1,10 +1,15 @@
-export const url = 'https://test.salesforce.com/';
-export const username = '$username';
-export const password = '$password';
-export const firstName = '$firstname';
-export const lastName = '$lastname';
-export const email = '$email';
-export const phone = '$phone';
-export const salutation = '$salutation';
-export const company = '$company';
-export const product = '$product';
+// export const url = 'https://test.salesforce.com/';
+// export const username = '$username';
+// export const password = '$password';
+// export const firstName = '$firstname';
+// export const lastName = '$lastname';
+// export const email = '$email';
+// export const phone = '$phone';
+// export const salutation = '$salutation';
+// export const company = '$company';
+// export const product = '$product';
+const temp = $TextfeildData
+const temp1 = $mobileNumber
+export const obj = JSON.parse(JSON.stringify(temp));
+export const obj1 = JSON.parse(JSON.stringify(temp1));
+
