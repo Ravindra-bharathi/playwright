fd8ab1d7742221fbf37504870f21f16a49c10ba8
Ravindra-bharathi <ravindrabharathi.a01@gmail.com>
2025-05-12 18:24:30 +0530

    worked on playwright

--- tests/activityFloating.ts
+++ tests/activityFloating.ts
@@ -1,10 +1,28 @@
+// export const url = 'https://test.salesforce.com/';
+// export const username = '$username';
+// export const password = '$password';
+// export const firstName = '$firstname';
+// export const lastName = '$lastname';
+// export const email = '$email';
+// export const phone = '$phone';
+// export const salutation = '$salutation';
+// export const company = '$company';
+// export const product = '$product';
+// export const stage = '$stage';
+// export const products = '$products'
+// export const quantity = '$quantity'
+
 export const url = 'https://test.salesforce.com/';
-export const username = '$username';
-export const password = '$password';
-export const firstName = '$firstname';
-export const lastName = '$lastname';
-export const email = '$email';
-export const phone = '$phone';
-export const salutation = '$salutation';
-export const company = '$company';
-export const product = '$product';
+export const username = 'vijayakumarp@basiscloudsolutions.com.floating';
+export const password = 'Basiscloud@1234';
+export const firstName = 'Ravindra';
+export const lastName = 'Bharathi';
+export const email = 'ravindrabharathia01@gmail.com';
+export const phone = '+919876543210';
+export const salutation = 'Mr.';
+export const company = 'BCS';
+export const product = 'Carpets';
+export const stage = 'Needs Analysis'
+export const Products = 'Heiring'
+export const quantity = '2'
+
