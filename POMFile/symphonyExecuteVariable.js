0d71c3bf513ad1a0b8fdd7ed19142a73e0ab3818
Ravindra-bharathi <ravindrabharathi.a01@gmail.com>
2025-05-13 10:23:36 +0530

    worked on playwright

--- POMFile/symphonyExecuteVariable.js
+++ POMFile/symphonyExecuteVariable.js
@@ -3,12 +3,10 @@ export const MSloginLink = "https://predev.symphony4cloud.com/login"
 
 export const usernamebox = "//input[@name ='loginfmt']"
 
-export const username = "mathankumar.selvan@businesscoresolutions.com"         //"$usermailid"
-
+export const username = "$usermailid"
 export const passwordbox = "//input[@name ='passwd']"
 
-export const password = "Dhayalan@1908"      // "$userpassword"
-
+export const password = "$userpassword"
 
 export const nextButton = "Next"
 
@@ -20,16 +18,11 @@ export const yesButton = "Yes"
 //symphony login-----------------------------------------------------------------------------------------
 
 export const customerCode = "// input[@name='customer_code']"
-export const customerId = "c100001"               //"$customerCode"        //"c100001"
-
+export const customerId = "$customerCode"
 export const userId = "//input[@id='user_name']"
-
-export const userIdIs = "mathankumarselvan"      //"$username"
-
-
+export const userIdIs = "$username"
 export const userPassword = "//input[@id='password']"
-export const userPasswordIs = "Dhayalan@1908"        //"$password"
-
+export const userPasswordIs = "$password"
 export const logInButton = "LOGIN"
 
 //preDev SymPhony HomePage ------------------------------------------------------------------------------
@@ -37,27 +30,26 @@ export const logInButton = "LOGIN"
 // click the drag and drop icon
 export const DragandDropButton = "Drag & Drop"
 
-export const clickTemplateList = "//h3[normalize-space()='Template List']"
-export const ClickSearchButton = "//input[@id='globalSearch']"
+export const clickTemplateList = "//h3[normalize-space()='Template List']"             //"Template List"
 
-export const templateID = "415096"    //"$tempid"  
+export const ClickSearchButton = "//input[@id='globalSearch']"  //"#globalSearch"
 
+export const templateID = "$tempid"               //"415053" // 415050 // 415044
 
-export const eyeButton = "//*[name()='path' and contains(@d,'M12 4.5C7 ')]"
+export const eyeButton = "//*[name()='path' and contains(@d,'M12 4.5C7 ')]" //"(//*[name()='path'])[105]"
 
-export const approveButton = "//button[@aria-label='Approve']"
+export const approveButton = "//button[@aria-label='Approve']"                                          //"button[aria-label='Approve'] svg"
 
-export const popUpLink = "Yes"
+export const popUpLink = "Yes" // //button[normalize-space()='Yes']
 
 export const tempscuccesfuly = "//*[name()='path' and contains(@d,'M12 2C6.47')]"
 export const backFromtemplate = "//button[normalize-space()='Back']//*[name()='svg']"
 
-export const ClickAuditTrail = "(//*[name()='svg'][@class='MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv'])[22]"
+export const ClickAuditTrail = "(//*[name()='svg'][@class='MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv'])[22]"       //"Audit Trail"
 
 
 export const clickinitiatedByBar = "//input[@name='action_user_name']"
-export const fillTheText3 = "mathankumar"  //"$Fill_Audit_Input"     
-
+export const fillTheText3 = "$Fill_Audit_Input"
 
 // validate the message will display or
 
@@ -70,18 +62,17 @@ export const backtobutton = "//*[name()='path' and contains(@d,'M11.67 3.8')]"
 
 // checked thestatus on DND Template in template status
 
-export const statusOfTemplate = "Published"
+export const statusOfTemplate = "Published" //"Published"
 
 // click the execute button
 export const ExecuteTheTemplate = "//button[@aria-label='Execute Job']"                       //"//*[name()='path' and contains(@d,'M12 2C6.48')]" // //button[@aria-label='Execute Job']
 
 // enter the job title and clcik the excute button
-export const jobTitleName = "#job_title"
-export const jobName = "test34"     //"$jobname"       
-
+export const jobTitleName = "#job_title" // //input[@id='job_title']
+export const jobName = "$jobname"
 export const executeButton = "//button[normalize-space()='Execute']"
 
-export const jobId = "(//div[@role='cell'])[2]"
+export const jobId = "(//div[@role='cell'])[2]" //"body > div:nth-child(4) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > main:nth-child(4) > div:nth-child(4) > div:nth-child(1) > div:nth-child(3) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2)"          //        //"body div[id='root'] div[role='presentation'] div[role='presentation'] div[role='rowgroup'] div:nth-child(1) div:nth-child(2) div:nth-child(1)"
 
 // click the template id and check the status
 
@@ -104,8 +95,12 @@ export const DNDTemplatbackButton = "//*[name()='path' and contains(@d,'M11.67 3
 
 // LogOut Operation------------------------------------------------------------------------------------------------------------------------------------------------
 
-export const DashboardButton = "//div[@aria-label='Dashboard']//*[name()='svg']"
+export const DashboardButton = "//div[@aria-label='Dashboard']//*[name()='svg']" //"Dashboard" // 
 
-export const profileIcon = "//div[text()='M']"
+export const profileIcon = "//div[text()='M']"          //"//button[@class='MuiButtonBase-root MuiIconButton-root MuiIconButton-colorInherit MuiIconButton-sizeMedium jss135 css-1deacqj']"
+//"//div[@class= 'MuiAvatar-root MuiAvatar-circular MuiAvatar-colorDefault jss132 css-16vgvkg']" //".MuiAvatar-root.MuiAvatar-circular.MuiAvatar-colorDefault.jss15552.css-16vgvkg" 
+// "(//div[@class='MuiAvatar-root MuiAvatar-circular MuiAvatar-colorDefault jss15552 css-16vgvkg'])[1]"
+export const logOutButton = "Log Out"  // "li:nth-child(4)"                          
 
-export const logOutButton = "Log Out"                          
+
+
